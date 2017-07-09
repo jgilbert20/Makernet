cfe6a146c70dbc2103f5a6c9cff6644f61d94371
Jeremy Gilbert <jgilbert@auspice.net>
2017-07-08 20:37:12 -0400

    Document new configuration variables

--- src/Globals.h
+++ src/Globals.h
@@ -17,12 +17,18 @@
 #define ADDR_UNASSIGNED 0x00
 #define ADDR_BROADCAST  0xFF
 
+
+// If true, support for controllers will be included in the runtime. There is
+// no harm in leaving this on all the time, but it contributes to code size.
+// As of July 8, disabling on a testbed is 6004 bytes, and enabling it
+// adds about 800 bytes.
 #define CONTROLLER_SUPPORT 1
 
-// This is used throughout the code for calculations of maximum payload size
+// This is used throughout the code for calculations of maximum payload size.
 #define MAX_MAKERNET_FRAME_LENGTH 25
 
-
+// If enabled, certain boundary and configuration checks are made at runtime
+// Slight increase to code size but makes errors much more sensible
 #define API_CHECK 0
 
 #endif
